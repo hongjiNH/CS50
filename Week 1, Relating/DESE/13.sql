ed30ada086f7d660321e67252f6c093e8744b630
hongjiNH <40945967+hongjiNH@users.noreply.github.com>
2025-03-31 18:32:36 +0800

    Delete Week 1, Relating directory

--- Week 1, Relating/DESE/13.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    "name" AS "name of the school",
-    "expenditures"."pupils"
-FROM
-    "schools"
-JOIN
-    "expenditures"
-ON
-    "expenditures"."district_id" = "schools"."district_id";
